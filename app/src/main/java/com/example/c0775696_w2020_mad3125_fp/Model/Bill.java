a98637656666210347ca291d22c20b35c2ea97e6
Jyothithomas <jyothithomas.05@gmail.com>
2020-04-12 22:12:33 -0400

    [JT] Created Model Classes

---
+++ app/src/main/java/com/example/c0775696_w2020_mad3125_fp/Model/Bill.java
@@ -0,0 +1,65 @@
+package com.example.c0775696_w2020_mad3125_fp.Model;
+
+import android.os.Parcelable;
+
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Date;
+
+public abstract class Bill implements Serializable {
+
+    public enum BillType {
+        Mobile,
+        Hydro,
+        Internet,
+    }
+    String billId;
+    LocalDate billDate;
+    BillType billType;
+    Double billTotal = 0.0;
+
+    Bill(String billId, LocalDate billDate, BillType billType)
+    {
+        this.billId = billId;
+        this.billDate = billDate;
+        this.billType = billType;
+    }
+
+    public Double billCalculate(){
+        double dummyAmount = 0.0;
+        return dummyAmount;
+    }
+
+    public String getBillId() {
+        return billId;
+    }
+
+    public void setBillId(String billId) {
+        this.billId = billId;
+    }
+
+    public LocalDate getBillDate() {
+        return billDate;
+    }
+
+    public void setBillDate(LocalDate billDate) {
+        this.billDate = billDate;
+    }
+
+    public BillType getBillType() {
+        return billType;
+    }
+
+    public void setBillType(BillType billType) {
+        this.billType = billType;
+    }
+
+    public Double getBillTotal() {
+        return billTotal;
+    }
+
+    public void setBillTotal(Double billTotal) {
+        this.billTotal = billTotal;
+    }
+}
---
+++ app/src/main/java/com/example/c0775696_w2020_mad3125_fp/Model/Hydro.java
@@ -0,0 +1,46 @@
+package com.example.c0775696_w2020_mad3125_fp.Model;
+
+import java.time.LocalDate;
+
+public class Hydro extends Bill {
+    private String agencyName;
+    private Integer unitsUsed;
+
+    public Hydro(String billId, LocalDate billDate, BillType billType, String agencyName, Integer unitsUsed){
+        super(billId, billDate, billType);
+        this.agencyName = agencyName;
+        this.unitsUsed = unitsUsed;
+        this.billTotal = billCalculate();
+    }
+
+    @Override
+    public Double billCalculate(){
+        double billAmount = 0.0;
+        if (unitsUsed < 10)
+        {
+            billAmount = 1.5 * unitsUsed;
+        }
+        else
+        {
+            billAmount = 2 * unitsUsed;
+        }
+        return billAmount;
+    }
+
+    public String getAgencyName() {
+        return agencyName;
+    }
+
+    public void setAgencyName(String agencyName) {
+        this.agencyName = agencyName;
+    }
+
+    public Integer getUnitsUsed() {
+        return unitsUsed;
+    }
+
+    public void setUnitsUsed(Integer unitsUsed) {
+        this.unitsUsed = unitsUsed;
+    }
+
+}
