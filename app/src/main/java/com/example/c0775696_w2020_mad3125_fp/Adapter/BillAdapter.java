e0e2f3bd486366428f06a7e40ed91efe67cda62e
Jyothithomas <jyothithomas.05@gmail.com>
2020-04-14 18:02:10 -0400

    [JT]  new ui of bills

--- app/src/main/java/com/example/c0775696_w2020_mad3125_fp/Adapter/BillAdapter.java
+++ app/src/main/java/com/example/c0775696_w2020_mad3125_fp/Adapter/BillAdapter.java
@@ -1,5 +1,6 @@
 package com.example.c0775696_w2020_mad3125_fp.Adapter;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,16 +10,18 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.c0775696_w2020_mad3125_fp.Activity.DetailedBillActivity;
 import com.example.c0775696_w2020_mad3125_fp.Model.Bill;
 import com.example.c0775696_w2020_mad3125_fp.R;
+import com.example.c0775696_w2020_mad3125_fp.Util.UtilMethods;
 
 import java.util.ArrayList;
 
-public class BillAdapter extends RecyclerView.Adapter<BillsAdapter.BillsViewHolder> {
+public class BillAdapter extends RecyclerView.Adapter<BillAdapter.BillsViewHolder> {
 
     public static ArrayList<Bill> billArrayList;
 
-    public BillsAdapter(ArrayList<Bill> billArrayList)
+    public BillAdapter(ArrayList<Bill> billArrayList)
     {
         this.billArrayList = billArrayList;
     }
@@ -31,6 +34,40 @@ public class BillAdapter extends RecyclerView.Adapter<BillsAdapter.BillsViewHold
                         , false);
         BillAdapter.BillsViewHolder mBillsViewHolder = new BillAdapter.BillsViewHolder(mView);
         return mBillsViewHolder;
+    }
+    @Override
+    public void onBindViewHolder(@NonNull BillsViewHolder holder, int position) {
+        Bill mBills = this.billArrayList.get(position);
+        if(mBills.getBillType().equals(Bill.BillType.Mobile))
+        {
+            holder.imgBillType.setImageResource(R.drawable.mobileicon);
+        }
+        if(mBills.getBillType().equals(Bill.BillType.Hydro)) {
+            holder.imgBillType.setImageResource(R.drawable.hydroicon);
+        }
+        if(mBills.getBillType().equals(Bill.BillType.Internet)) {
+            holder.imgBillType.setImageResource(R.drawable.interneticon);
+        }
+        holder.textBillID.setText(mBills.getBillId());
+        holder.textBillAmount.setText(UtilMethods.getInstance().doubleFormatter(mBills.getBillTotal()));
+        holder.textBillDate.setText(mBills.getBillDate().toString());
+        holder.textBillType.setText(mBills.getBillType().toString());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bill billObj = billArrayList.get(position);
+
+                Intent mIntent = new Intent(holder.itemView.getContext(), DetailedBillActivity.class);
+                mIntent.putExtra("Bills", billObj);
+                holder.itemView.getContext().startActivity(mIntent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.billArrayList.size();
     }
     public class BillsViewHolder extends RecyclerView.ViewHolder {
         TextView textBillID,textBillDate,textBillType,textBillAmount;
