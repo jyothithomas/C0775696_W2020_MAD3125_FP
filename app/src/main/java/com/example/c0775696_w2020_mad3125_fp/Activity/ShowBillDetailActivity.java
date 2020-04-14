651ab2859f6e935a70091e9527f9fa8f5ae11f98
Jyothithomas <jyothithomas.05@gmail.com>
2020-04-14 17:12:17 -0400

    [JT] added Adapter class for bill recycler view

--- app/src/main/java/com/example/c0775696_w2020_mad3125_fp/Activity/ShowBillDetailActivity.java
+++ app/src/main/java/com/example/c0775696_w2020_mad3125_fp/Activity/ShowBillDetailActivity.java
@@ -1,12 +1,23 @@
 package com.example.c0775696_w2020_mad3125_fp.Activity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.c0775696_w2020_mad3125_fp.Adapter.BillAdapter;
 import com.example.c0775696_w2020_mad3125_fp.R;
 
+import java.util.ArrayList;
+
 public class ShowBillDetailActivity extends AppCompatActivity {
+    private RecyclerView rvBillsList;
+    private ArrayList billsArrayList;
+    private BillAdapter billsAdapter;
+    private ImageView imgAddButton;
+    private TextView txtTotalAmountValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/example/c0775696_w2020_mad3125_fp/Adapter/BillAdapter.java
@@ -0,0 +1,4 @@
+package com.example.c0775696_w2020_mad3125_fp.Adapter;
+
+public class BillAdapter  {
+}
