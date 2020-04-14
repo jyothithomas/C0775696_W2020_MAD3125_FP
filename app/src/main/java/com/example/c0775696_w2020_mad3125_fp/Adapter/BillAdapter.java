4b4be7f3e4537898be318772df6fd5e3cbfd4ac2
Jyothithomas <jyothithomas.05@gmail.com>
2020-04-14 17:43:49 -0400

    [JT] adding Bill details in new ui of bills

--- app/src/main/java/com/example/c0775696_w2020_mad3125_fp/Adapter/BillAdapter.java
+++ app/src/main/java/com/example/c0775696_w2020_mad3125_fp/Adapter/BillAdapter.java
@@ -3,6 +3,8 @@ package com.example.c0775696_w2020_mad3125_fp.Adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -30,4 +32,18 @@ public class BillAdapter extends RecyclerView.Adapter<BillsAdapter.BillsViewHold
         BillAdapter.BillsViewHolder mBillsViewHolder = new BillAdapter.BillsViewHolder(mView);
         return mBillsViewHolder;
     }
+    public class BillsViewHolder extends RecyclerView.ViewHolder {
+        TextView textBillID,textBillDate,textBillType,textBillAmount;
+        ImageView imgBillType;
+        public BillsViewHolder(@NonNull View itemView)
+        {
+            super(itemView);
+            textBillID = itemView.findViewById(R.id.txtBillId);
+            textBillDate = itemView.findViewById(R.id.txtBillDate);
+            textBillType = itemView.findViewById(R.id.txtBillType);
+            textBillAmount = itemView.findViewById(R.id.txtTotalAmount);
+            imgBillType = itemView.findViewById(R.id.imgBillType);
+        }
+    }
+
 }
