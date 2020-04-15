68933be4bd68beaf7aa545298be09dac465ca1a8
Jyothithomas <jyothithomas.05@gmail.com>
2020-04-15 11:20:52 -0400

    [JT]  adding menu add button

--- app/src/main/java/com/example/c0775696_w2020_mad3125_fp/Activity/ShowBillDetailActivity.java
+++ app/src/main/java/com/example/c0775696_w2020_mad3125_fp/Activity/ShowBillDetailActivity.java
@@ -69,6 +69,9 @@ public class ShowBillDetailActivity extends AppCompatActivity {
                 startActivity(logout);
                 finish();
                 return true;
+            case R.id.btnAdd:
+                startActivity(new Intent(ShowBillDetailActivity.this, AddNewBillActivity.class));
+                //CustomerListActivity.this.finish();
         }
         return true;
     }
