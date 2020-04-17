a4769d7deee181060cb4500100fe30d96c14cf23
Jyothithomas <jyothithomas.05@gmail.com>
2020-04-17 11:48:39 -0400

    [JT] Updated more objects for customer and bills

--- app/src/main/java/com/example/c0775696_w2020_mad3125_fp/Util/DataSingelton.java
+++ app/src/main/java/com/example/c0775696_w2020_mad3125_fp/Util/DataSingelton.java
@@ -24,10 +24,19 @@ public class DataSingelton {
     {
         Customer c1 = new Customer("C01","Jyothi","Thomas",
                 "Female","jyothi@mail.com","joe123","joe123","Scarborough",
-                "01/Jan/1991", R.drawable.women);
+                "05/Sep/1990", R.drawable.women);
         Customer c2 = new Customer("C02","James","Thomas",
                 "Male","james@mail.com","jamz123","jamz123","Kerala",
-                "01/Aug/1988",R.drawable.man);
+                "21/Aug/1988",R.drawable.man);
+        Customer c3 = new Customer("C03","Raelyn","Remy",
+                "Female","rae_lyn@mail.com","rae_lyn12","raelyn123","Kollam",
+                "05/Jul/2018",R.drawable.women);
+        Customer c4 = new Customer("C04","Avani","Patel",
+                "Female","Avipt@mail.com","avanipt93","avanipt1993","Guelph",
+                "11/June/1993",R.drawable.women);
+        Customer c5 = new Customer("C05","Charmi","Patel",
+                "Female","charmipt@mail.com","charpt123","chrmi_123","Brampton",
+                "01/Aug/1993",R.drawable.women);
         Mobile m1 = new Mobile("MB01",
                 "09/Oct/2010",
                 Bill.BillType.Mobile,
@@ -36,24 +45,46 @@ public class DataSingelton {
                 "4372435081",
                 3,
                 12);
+        Mobile m2 = new Mobile("MB02",
+                "03/Jun/2011",
+                Bill.BillType.Mobile,
+                "Samsung",
+                "International",
+                "123456789",
+                6,
+                20);
         Hydro h1 = new Hydro( "HY01",
                 "12/Dec/2012",
                 Bill.BillType.Hydro,
                 "ElectroHydro",
                 18);
+        Hydro h2 = new Hydro( "HY02",
+                "10/Apr/2015",
+                Bill.BillType.Hydro,
+                "HydroCanada",
+                25);
         Internet in1 = new Internet("IN01",
                "11/Jun/2013",
                 Bill.BillType.Internet,
                 "Rogers",
                 21.3);
+        Internet in2 = new Internet("IN02",
+                "30/Jan/2013",
+                Bill.BillType.Internet,
+                "Bell",
+                11.5);
         c2.addBill("IN01", in1);
         c1.addBill("MB01", m1);
         c1.addBill("HY01", h1);
+        c3.addBill("HY01",h1);
+        c3.addBill("IN02",in2);
+        c3.addBill("HY02",h2);
+        c4.addBill("MB01",m1);
+        c4.addBill("MB02",m2);
         customerMap.put(c1.getCustomerId(),c1);
         customerMap.put(c2.getCustomerId(),c2);
-        //customerMap.put(c3.getCustomerId(),c3);
-       // customerMap.put(c4.getCustomerId(),c4);
-       // customerMap.put(c5.getCustomerId(),c5);
-        //customerMap.put(c6.getCustomerId(),c6);
+        customerMap.put(c3.getCustomerId(),c3);
+        customerMap.put(c4.getCustomerId(),c4);
+        customerMap.put(c5.getCustomerId(),c5);
     }
 }
