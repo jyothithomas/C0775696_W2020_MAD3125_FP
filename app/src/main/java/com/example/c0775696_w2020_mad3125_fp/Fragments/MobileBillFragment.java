a8e97ecdcfe114b10af900668746e4cf3a24daa0
Jyothithomas <jyothithomas.05@gmail.com>
2020-04-17 16:33:22 -0400

    [JT] Adding Fragments to show bill details

--- app/src/main/java/com/example/c0775696_w2020_mad3125_fp/Fragments/MobileBillFragment.java
+++ app/src/main/java/com/example/c0775696_w2020_mad3125_fp/Fragments/MobileBillFragment.java
@@ -4,61 +4,43 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.c0775696_w2020_mad3125_fp.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MobileBillFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class MobileBillFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class MobileBillFragment extends Fragment {
+
+     private TextView txtFragBillId, txtFragBillDate, txtFragDataUsed, txtFragManufac, txtFragPlanName, txtFragMinsUsed, txtFragBillAmount;
+
 
     public MobileBillFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MobileBillFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MobileBillFragment newInstance(String param1, String param2) {
-        MobileBillFragment fragment = new MobileBillFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
+
         return inflater.inflate(R.layout.fragment_mobile_bill, container, false);
+
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        txtFragBillId = view.findViewById(R.id.txtFragBillId);
+        txtFragBillDate = view.findViewById(R.id.txtFragBillDate);
+        txtFragDataUsed = view.findViewById(R.id.txtFragDataUsed);
+        txtFragManufac = view.findViewById(R.id.txtFragManufac);
+        txtFragPlanName = view.findViewById(R.id.txtFragPlanName);
+        txtFragMinsUsed = view.findViewById(R.id.txtFragMinsUsed);
+        txtFragBillAmount = view.findViewById(R.id.txtBillAmount);
+
+    }
+
 }
