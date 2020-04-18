84d712cf35cb0454c86783c9c6f7583f9b27904d
Jyothithomas <jyothithomas.05@gmail.com>
2020-04-18 17:39:48 -0400

    [JT] Detailed bill view obtained

--- app/src/main/java/com/example/c0775696_w2020_mad3125_fp/Activity/DetailedBillActivity.java
+++ app/src/main/java/com/example/c0775696_w2020_mad3125_fp/Activity/DetailedBillActivity.java
@@ -6,9 +6,6 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import com.example.c0775696_w2020_mad3125_fp.Model.Bill;
 import com.example.c0775696_w2020_mad3125_fp.Model.Hydro;
@@ -17,14 +14,10 @@ import com.example.c0775696_w2020_mad3125_fp.Model.Mobile;
 import com.example.c0775696_w2020_mad3125_fp.R;
 
 public class DetailedBillActivity extends AppCompatActivity {
-
     Bill billObj;
     Mobile mob;
     Hydro hydro;
     Internet internet;
-
-    FragmentManager mFragmentManager;
-    FragmentTransaction mFragmentTransaction;
     TextView tv1,tv2,tv4,tv5,tv6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,18 +28,10 @@ public class DetailedBillActivity extends AppCompatActivity {
         tv4 = findViewById(R.id.tv4);
         tv5 = findViewById(R.id.tv5);
         tv6 = findViewById(R.id.tv6);
-
-
         Intent mIntent = getIntent();
         // Bill billObj = (Bill) mIntent.getSerializableExtra("Bills");
         ActionBar mActionBar = getSupportActionBar();
         mActionBar.hide();
-
-        //mFragmentManager = getSupportFragmentManager();
-        mFragmentManager = getSupportFragmentManager();
-        mFragmentTransaction = mFragmentManager.beginTransaction();
-        Fragment mFragment = null;
-
         if (getIntent().getExtras() != null) {
             billObj = (Bill) mIntent.getSerializableExtra("Bills");
             if (billObj.getBillType().equals(Bill.BillType.Mobile)) {
@@ -70,7 +55,11 @@ public class DetailedBillActivity extends AppCompatActivity {
             if(billObj.getBillType().equals(Bill.BillType.Internet))
             {
                 internet = (Internet)mIntent.getSerializableExtra("Bills");
-
+                tv5.setText("Bill ID::::"+billObj.getBillId());
+                tv1.setText("Bill Date::::"+billObj.getBillDate());
+                tv2.setText("Bill Type::::"+billObj.getBillType().toString());
+                tv6.setText("Internet Provider::::"+internet.getProviderName());
+                tv4.setText("GB Used::::"+internet.getGbUsed().toString());
             }
         }
 
