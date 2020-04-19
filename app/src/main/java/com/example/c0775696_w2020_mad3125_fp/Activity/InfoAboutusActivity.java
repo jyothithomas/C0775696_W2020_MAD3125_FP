d87a888b6052bd7c58f5203442f80aecbf21496b
Jyothithomas <jyothithomas.05@gmail.com>
2020-04-18 21:00:58 -0400

    [JT] Adding about us HTML feature using WebView

--- app/src/main/java/com/example/c0775696_w2020_mad3125_fp/Activity/InfoAboutusActivity.java
+++ app/src/main/java/com/example/c0775696_w2020_mad3125_fp/Activity/InfoAboutusActivity.java
@@ -1,6 +1,7 @@
 package com.example.c0775696_w2020_mad3125_fp.Activity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +13,8 @@ public class InfoAboutusActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_aboutus);
+        String htmlAsString = getString(R.string.html);
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.loadDataWithBaseURL(null, htmlAsString, "text/html", "utf-8", null);
     }
 }
